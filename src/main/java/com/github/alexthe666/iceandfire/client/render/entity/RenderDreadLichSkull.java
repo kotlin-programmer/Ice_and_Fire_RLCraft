e95b1567e4efffb770034b35716927dfe8c1eeb9
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-08-12 16:44:20 +0200

    Fix dread lich skull renderer corrupting opengl state

--- src/main/java/com/github/alexthe666/iceandfire/client/render/entity/RenderDreadLichSkull.java
+++ src/main/java/com/github/alexthe666/iceandfire/client/render/entity/RenderDreadLichSkull.java
@@ -22,16 +22,13 @@ public class RenderDreadLichSkull extends Render<EntityDreadLichSkull> {
 	@Override
 	public void doRender(EntityDreadLichSkull entity, double x, double y, double z, float entityYaw, float partialTicks) {
 		GlStateManager.pushMatrix();
+
 		GlStateManager.translate(x, y, z);
-		GlStateManager.pushMatrix();
 		GlStateManager.scale(1.0F, -1.0F, 1.0F);
 		GlStateManager.rotate(interpolateRotation(entity.prevRotationYaw, entity.rotationYaw, partialTicks) - 180.0F, 0.0F, 1.0F, 0.0F);
 
 		GlStateManager.disableCull();
 		GlStateManager.enableRescaleNormal();
-		GlStateManager.enableAlpha();
-		GlStateManager.enableBlend();
-		GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
 		GlStateManager.disableLighting();
 		if (this.renderOutlines) {
 			GlStateManager.enableColorMaterial();
@@ -43,18 +40,15 @@ public class RenderDreadLichSkull extends Render<EntityDreadLichSkull> {
 		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 0.0F);
 		MODEL.render(entity, 0.0F, 0.0F, entity.ticksExisted + partialTicks, 0.0F, 0.0F, 0.0625F);
 
-		GlStateManager.enableLighting();
-		GlStateManager.disableBlend();
+		GlStateManager.enableCull();
+		GlStateManager.disableRescaleNormal();
 		GlStateManager.enableLighting();
 		if (this.renderOutlines) {
-			GlStateManager.disableOutlineMode();
 			GlStateManager.disableColorMaterial();
+			GlStateManager.disableOutlineMode();
 		}
 
 		GlStateManager.popMatrix();
-		GlStateManager.popMatrix();
-
-		super.doRender(entity, x, y, z, entityYaw, partialTicks);
 	}
 
 	private static float interpolateRotation(float prevYawOffset, float yawOffset, float partialTicks) {
