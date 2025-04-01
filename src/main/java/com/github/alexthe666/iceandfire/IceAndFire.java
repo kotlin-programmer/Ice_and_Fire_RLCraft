d5b6b224c44f24cd6dce2b3ba6364cc6f1b32bac
kotlin-programmer <seattlesoftwareengineer@gmail.com>
2025-04-01 01:01:49 -0700

    Bump version to 2.1.2

--- src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
+++ src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
@@ -53,7 +53,7 @@ import java.util.Random;
 public class IceAndFire {
 
     public static final String MODID = "iceandfire";
-    public static final String VERSION = "2.1.1";
+    public static final String VERSION = "2.1.2";
     public static final String LLIBRARY_VERSION = "1.7.9";
     public static final String NAME = "Ice And Fire";
     public static final Logger logger = LogManager.getLogger(NAME);
