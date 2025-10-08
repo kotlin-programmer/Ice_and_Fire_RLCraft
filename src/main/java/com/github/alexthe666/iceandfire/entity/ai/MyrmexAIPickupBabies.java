a59a8665e8cd0771e8467d873eb8e6cdd97c112c
Nischhelm <nischhelm@gmail.com>
2025-10-08 19:40:22 +0200

    only cleanup for some myrmex ai
    and slight performance increase

--- src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAIPickupBabies.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAIPickupBabies.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class MyrmexAIPickupBabies<T extends EntityItem> extends EntityAITarget {
+public class MyrmexAIPickupBabies extends EntityAITarget {
     protected final DragonAITargetItems.Sorter theNearestAttackableTargetSorter;
     protected final Predicate<? super EntityLivingBase> targetEntitySelector;
     protected EntityLivingBase targetEntity;
@@ -24,12 +24,7 @@ public class MyrmexAIPickupBabies<T extends EntityItem> extends EntityAITarget {
     public MyrmexAIPickupBabies(EntityMyrmexWorker myrmex) {
         super(myrmex, false, false);
         this.theNearestAttackableTargetSorter = new DragonAITargetItems.Sorter(myrmex);
-        this.targetEntitySelector = new Predicate<EntityLivingBase>() {
-            @Override
-            public boolean apply(@Nullable EntityLivingBase myrmex) {
-                return myrmex != null && (myrmex instanceof EntityMyrmexBase && ((EntityMyrmexBase)myrmex).getGrowthStage() < 2 && !((EntityMyrmexBase)myrmex).isInNursery() || myrmex instanceof EntityMyrmexEgg && !((EntityMyrmexEgg)myrmex).isInNursery());
-            }
-        };
+        this.targetEntitySelector = other -> other != null && (other instanceof EntityMyrmexBase && ((EntityMyrmexBase) other).getGrowthStage() < 2 && !((EntityMyrmexBase) other).isInNursery() || other instanceof EntityMyrmexEgg && !((EntityMyrmexEgg) other).isInNursery());
         this.myrmex = myrmex;
         this.setMutexBits(1);
     }
@@ -39,11 +34,11 @@ public class MyrmexAIPickupBabies<T extends EntityItem> extends EntityAITarget {
         if (!this.myrmex.canMove() || !this.myrmex.getNavigator().noPath() || this.myrmex.shouldEnterHive() || !this.myrmex.keepSearching || this.myrmex.holdingBaby()) {
             return false;
         }
-        List<EntityLivingBase> listBabies = this.taskOwner.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class, this.getTargetableArea(this.getTargetDistance()), this.targetEntitySelector);
+        List<EntityLivingBase> listBabies = this.taskOwner.world.getEntitiesWithinAABB(EntityLivingBase.class, this.getTargetableArea(this.getTargetDistance()), this.targetEntitySelector);
         if (listBabies.isEmpty()) {
             return false;
         } else {
-            Collections.sort(listBabies, this.theNearestAttackableTargetSorter);
+            listBabies.sort(this.theNearestAttackableTargetSorter);
             this.targetEntity = listBabies.get(0);
             return true;
         }
@@ -62,7 +57,7 @@ public class MyrmexAIPickupBabies<T extends EntityItem> extends EntityAITarget {
     @Override
     public void updateTask() {
         super.updateTask();
-        if (this.targetEntity == null || this.targetEntity != null && this.targetEntity.isDead) {
+        if (this.targetEntity == null || this.targetEntity.isDead) {
             this.resetTask();
         }
         if (this.targetEntity != null && !this.targetEntity.isDead && this.taskOwner.getDistanceSq(this.targetEntity) < 2) {
@@ -74,19 +69,5 @@ public class MyrmexAIPickupBabies<T extends EntityItem> extends EntityAITarget {
     @Override
     public boolean shouldContinueExecuting() {
         return !this.taskOwner.getNavigator().noPath();
-    }
-
-    public static class Sorter implements Comparator<Entity> {
-        private final Entity theEntity;
-
-        public Sorter(EntityMyrmexBase theEntityIn) {
-            this.theEntity = theEntityIn;
-        }
-
-        public int compare(Entity p_compare_1_, Entity p_compare_2_) {
-            double d0 = this.theEntity.getDistanceSq(p_compare_1_);
-            double d1 = this.theEntity.getDistanceSq(p_compare_2_);
-            return d0 < d1 ? -1 : (d0 > d1 ? 1 : 0);
-        }
     }
 }
