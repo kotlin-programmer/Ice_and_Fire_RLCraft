bc37005d2e0a37fae41eef70672526568196b9f4
Nischhelm <nischhelm@gmail.com>
2025-10-08 19:36:43 +0200

    myrmex storing items
    - only store when on resin (dont try to store when outside the hive)
    - cleanup

--- src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAIStoreBabies.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAIStoreBabies.java
@@ -5,13 +5,11 @@ import com.github.alexthe666.iceandfire.entity.util.MyrmexHive;
 import com.github.alexthe666.iceandfire.structures.WorldGenMyrmexHive;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.EntityAIBase;
-import net.minecraft.pathfinding.Path;
 import net.minecraft.util.math.BlockPos;
 
 public class MyrmexAIStoreBabies extends EntityAIBase {
     private final EntityMyrmexWorker myrmex;
     private final double movementSpeed;
-    private Path path;
     private BlockPos nextRoom = BlockPos.ORIGIN;
 
     public MyrmexAIStoreBabies(EntityMyrmexWorker entityIn, double movementSpeedIn) {
@@ -21,7 +19,7 @@ public class MyrmexAIStoreBabies extends EntityAIBase {
     }
 
     public boolean shouldExecute() {
-        if (!this.myrmex.canMove() || !this.myrmex.holdingBaby() || !this.myrmex.shouldEnterHive() && !this.myrmex.getNavigator().noPath() || this.myrmex.canSeeSky()) {
+        if (!this.myrmex.canMove() || !this.myrmex.holdingBaby() || !this.myrmex.shouldEnterHive() && !this.myrmex.getNavigator().noPath() || this.myrmex.canSeeSky() || !this.myrmex.isOnResin()) {
             return false;
         }
         MyrmexHive village = this.myrmex.getHive();
@@ -37,9 +35,6 @@ public class MyrmexAIStoreBabies extends EntityAIBase {
         return this.myrmex.holdingBaby() && !this.myrmex.getNavigator().noPath() && this.myrmex.getDistanceSq(nextRoom) > 3 && this.myrmex.shouldEnterHive();
     }
 
-    public void startExecuting() {
-    }
-
     @Override
     public void updateTask() {
         this.myrmex.getNavigator().tryMoveToXYZ(this.nextRoom.getX(), this.nextRoom.getY(), this.nextRoom.getZ(), this.movementSpeed);
@@ -47,16 +42,10 @@ public class MyrmexAIStoreBabies extends EntityAIBase {
             if(!this.myrmex.getPassengers().isEmpty()){
                 for(Entity entity : this.myrmex.getPassengers()){
                     entity.dismountRidingEntity();
-                    resetTask();
+                    this.myrmex.getNavigator().clearPath();
                     entity.copyLocationAndAnglesFrom(this.myrmex);
                 }
             }
         }
     }
-
-    public void resetTask() {
-        nextRoom = BlockPos.ORIGIN;
-        this.myrmex.getNavigator().setPath(null, this.movementSpeed);
-    }
-
 }
