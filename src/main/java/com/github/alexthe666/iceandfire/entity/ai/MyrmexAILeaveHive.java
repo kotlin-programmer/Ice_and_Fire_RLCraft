c52048e009b89ee8c6ed2c7b7d1fe5b36683f8fa
Nischhelm <nischhelm@gmail.com>
2025-10-11 00:09:19 +0200

    leave ai
    that was complicated af to get right
    prob still not perfect (what if theres no path out?)

--- src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAILeaveHive.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAILeaveHive.java
@@ -5,13 +5,12 @@ import com.github.alexthe666.iceandfire.entity.util.MyrmexHive;
 import com.github.alexthe666.iceandfire.world.MyrmexWorldData;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.pathfinding.Path;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 
 public class MyrmexAILeaveHive extends EntityAIBase {
     private final EntityMyrmexBase myrmex;
     private final double movementSpeed;
-    private BlockPos nextEntrance = BlockPos.ORIGIN;
+    private int delay = 0;
 
     public MyrmexAILeaveHive(EntityMyrmexBase entityIn, double movementSpeedIn) {
         this.myrmex = entityIn;
@@ -20,29 +19,37 @@ public class MyrmexAILeaveHive extends EntityAIBase {
     }
 
     public boolean shouldExecute() {
-        if(this.myrmex.isChild()){
-            return false;
-        }
-
-        if(!this.myrmex.canMove() || !this.myrmex.shouldLeaveHive() || !this.myrmex.isOnResin() || this.myrmex instanceof EntityMyrmexWorker && (((EntityMyrmexWorker)this.myrmex).holdingBaby() || !this.myrmex.getHeldItem(EnumHand.MAIN_HAND).isEmpty()) || this.myrmex.isEnteringHive) {
+        if(delay > 0) delay--;
+        if(delay > 0 || !this.myrmex.canMove() || !this.myrmex.shouldLeaveHive() || !this.myrmex.isOnResin() || !this.myrmex.getNavigator().noPath() || this.myrmex.isEnteringHive) {
             return false;
         }
         MyrmexHive village = MyrmexWorldData.get(this.myrmex.world).getNearestHive(new BlockPos(this.myrmex), 1000);
         if (village == null) {
             return false;
         } else {
-            nextEntrance = MyrmexHive.getGroundedPos(this.myrmex.world, village.getClosestEntranceToEntity(this.myrmex, this.myrmex.getRNG(), true));
+            BlockPos nextEntrance = MyrmexHive.getGroundedPos(this.myrmex.world, village.getClosestEntranceToEntity(this.myrmex, this.myrmex.getRNG(), true));
             Path path = this.myrmex.getNavigator().getPathToPos(nextEntrance);
-            this.myrmex.getNavigator().setPath(path, this.movementSpeed);
-            this.myrmex.isEnteringHive = false;
+            if(path == null) {
+                //fallback 1: path to bottom of entrance
+                nextEntrance = village.getClosestEntranceBottomToEntity(this.myrmex, this.myrmex.getRNG());
+                path = this.myrmex.getNavigator().getPathToPos(nextEntrance);
+                if(path == null) {
+                    //fallback 2: path to hive center
+                    nextEntrance = village.getCenterGround();
+                    path = this.myrmex.getNavigator().getPathToPos(nextEntrance);
+                }
+            }
+            if(path != null) {
+                this.myrmex.getNavigator().setPath(path, this.movementSpeed);
+                this.myrmex.isEnteringHive = false;
+            } else {
+                delay = 100; //allow the myrmex to do other tasks, maybe it will find the way later
+            }
             return path != null;
         }
     }
 
     public boolean shouldContinueExecuting() {
-        if(this.myrmex.getDistanceSq(nextEntrance) <= 3 || this.myrmex.shouldEnterHive()) {
-            return false;
-        }
-        return !this.myrmex.getNavigator().noPath() && this.myrmex.getDistanceSq(nextEntrance) > 3 && this.myrmex.shouldLeaveHive();
+        return !this.myrmex.getNavigator().noPath();
     }
 }
