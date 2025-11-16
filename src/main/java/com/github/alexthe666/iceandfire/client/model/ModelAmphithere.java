35fef77a93b32c64dd1d65980dac552a8f3344ea
Iquryy <168448556+Iquryy@users.noreply.github.com>
2025-11-16 11:47:37 +0200

    Amphithere Model Fix
    needed?

--- src/main/java/com/github/alexthe666/iceandfire/client/model/ModelAmphithere.java
+++ src/main/java/com/github/alexthe666/iceandfire/client/model/ModelAmphithere.java
@@ -227,7 +227,7 @@ public class ModelAmphithere extends ModelDragonBase {
         this.Neck3.setRotationPoint(0.0F, 0.0F, -5.1F);
         this.Neck3.addBox(-1.5F, -1.1F, -7.0F, 3, 4, 8, 0.0F);
         this.setRotateAngle(Neck3, -0.18203784098300857F, -0.0F, 0.0F);
-        this.Tail4 = new AdvancedModelRenderer(this, 70, 22);
+        this.Tail4 = new AdvancedModelRenderer(this, 70, 21);
         this.Tail4.setRotationPoint(0.0F, 0.3F, 9.0F);
         this.Tail4.addBox(-1.02F, -1.3F, -0.6F, 2, 2, 9, 0.0F);
         this.setRotateAngle(Tail4, -0.045553093477052F, 0.0F, 0.0F);
@@ -290,6 +290,9 @@ public class ModelAmphithere extends ModelDragonBase {
         this.Neck3.setScale(0.99F, 0.99F, 0.99F);
         animator = ModelAnimator.create();
         this.updateDefaultPose();
+        this.TailL2.setRotationPoint(-2.0F, 0.7F, 1.7F);
+        this.TailL2.defaultOffsetZ = 0.03735f;
+        this.TailL2.defaultOffsetX = 0.01075f;
     }
 
     @Override
