474ebbef7da07de9c36eddbe17f7e56a0015ccf6
Nischhelm <161029624+Nischhelm@users.noreply.github.com>
2026-05-09 09:58:28 +0200

    Dragonforge disallow (#60)
    * disallow opening gui if forge built wrong (code for it was already in vent block but not in aperture block)
    * draw x if not assembled

--- src/main/java/com/github/alexthe666/iceandfire/client/gui/GuiDragonforge.java
+++ src/main/java/com/github/alexthe666/iceandfire/client/gui/GuiDragonforge.java
@@ -1,7 +1,6 @@
 package com.github.alexthe666.iceandfire.client.gui;
 
 import com.github.alexthe666.iceandfire.entity.tile.TileEntityDragonforge;
-import com.github.alexthe666.iceandfire.enums.EnumDragonType;
 import com.github.alexthe666.iceandfire.inventory.ContainerDragonforge;
 import com.github.alexthe666.iceandfire.item.IafItemRegistry;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -54,13 +53,17 @@ public class GuiDragonforge extends GuiContainer {
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-        int i1 = this.func_175381_h(126);
+        int i1 = this.getCookProgressScaled(126);
         this.drawTexturedModalRect(k + 12, l + 23, 0, 166, i1, 38);
+
+        if(tileFurnace instanceof TileEntityDragonforge && !((TileEntityDragonforge) this.tileFurnace).assembled()) {
+            this.drawTexturedModalRect(k + 119, l + 35, 126, 166, 15, 15);
+        }
     }
 
-    private int func_175381_h(int p_175381_1_) {
+    private int getCookProgressScaled(int pixels) {
         int j = this.tileFurnace.getField(0);
-        return j != 0 ? j * p_175381_1_ / ((TileEntityDragonforge) tileFurnace).getMaxCookTime() : 0;
+        return j != 0 ? j * pixels / ((TileEntityDragonforge) tileFurnace).getMaxCookTime() : 0;
     }
 
     @Override
